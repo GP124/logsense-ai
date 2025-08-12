9abef9dedca54e317e02efa9d4a5914a37a2910a
guruprakash124 <guru.prakash12499@gmail.com>
2025-08-12 16:23:26 +0530

    Initial commit

---
+++ src/main/java/com/adf/logsense_ai/LogsenseAiApplication.java
@@ -0,0 +1,13 @@
+package com.adf.logsense_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsenseAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsenseAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adf/logsense_ai/LogsenseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adf.logsense_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsenseAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
